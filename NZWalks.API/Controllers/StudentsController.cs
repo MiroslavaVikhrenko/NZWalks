3146c31ca2fbf0d5278384fd3def2f8bb10304be
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-11 16:24:03 -0700

    Test a new action method GetAllStudents in a new controller Students

--- NZWalks.API/Controllers/StudentsController.cs
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -9,5 +9,13 @@ namespace NZWalks.API.Controllers
     public class StudentsController : ControllerBase
     {
         //add action methods
+
+        //GET https://localhost:portnumber/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "John", "Jane", "Mark", "Emily", "David" }; //only for simplicity
+            return Ok(studentNames);
+        }
     }
 }
