7ec705c64e726be553a48f15d8becd101ea83c2f
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-11 17:25:30 -0700

    Inject DbContext in Program.cs and pass a connection string

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,11 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. (Inject services)
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Inject our db context class
+//builder.Services collection
+//options.UseSqlServer from Microsoft.EntityFrameworkCore, passing the connection string from appsettings.json
+
+
+builder.Services.AddDbContext<NZWalksDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString"))); 
 
 var app = builder.Build();
 
