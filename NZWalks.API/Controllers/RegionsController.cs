b49318bed149d66bff4b7b527c2ea73fbbe0a176
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-11 18:04:08 -0700

    Add an action method to get all regions

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -1,11 +1,42 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
 
 namespace NZWalks.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
+    //https://localhost:portnumber/api/regions
+    [Route("api/[controller]")] //defining the route whenever a user enters this route along with the app URL,
+                                //it will be pointed to the RegionsController
+    [ApiController] //<= This attribute will tell the app that this controller is for API use
+
     public class RegionsController : ControllerBase
     {
+        //create action methods
+
+        //Method to return all regions
+        // GET: https://localhost:portnumber/api/regions (RESTful URL)
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Auckland Region",
+                    Code = "AKL",
+                    RegionImageUrl = "https://www.pexels.com/photo/silhouetted-couple-exploring-piha-archway-29249172/"
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Wellington Region",
+                    Code = "WLG",
+                    RegionImageUrl = "https://www.pexels.com/photo/brown-hanging-bridge-surrounded-by-trees-403781/"
+                }
+            };
+
+            return Ok(regions);
+        }
     }
 }
