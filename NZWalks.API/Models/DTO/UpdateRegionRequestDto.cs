0a03a226c9c8705b68bee67b69ec0b2c11e30d5a
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-12 15:32:43 -0700

    Add UpdateRegionRequest DTO for update region requests

---
+++ NZWalks.API/Models/DTO/UpdateRegionRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace NZWalks.API.Models.DTO
+{
+    public class UpdateRegionRequestDto
+    {
+        //this class will have only those properties that we want the client to update
+        //we do not want to allow the client to update an Id
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+    }
+}
