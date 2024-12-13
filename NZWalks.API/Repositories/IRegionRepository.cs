cbf133cee50a9808ce25fcec5bce62530f901605
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-12 17:49:01 -0700

    Add GetById() method to IRegionRepository

--- NZWalks.API/Repositories/IRegionRepository.cs
+++ NZWalks.API/Repositories/IRegionRepository.cs
@@ -6,5 +6,7 @@ namespace NZWalks.API.Repositories
     {
         //definitions of the 5 methods that we want to expose and later implement in a concrete class
         Task<List<Region>> GetAllAsync(); //returns a list of region domain models
+
+        Task<Region?> GetById(Guid id); //Region can be null
     }
 }
