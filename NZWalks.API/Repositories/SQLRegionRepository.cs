8e6698388ff0b4884a3a9f71c75027de1ecbad99
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-12 17:11:43 -0700

    Inject db context class to SQLRegionRepository class and add implemenation for GetAllAsync() method

--- NZWalks.API/Repositories/SQLRegionRepository.cs
+++ NZWalks.API/Repositories/SQLRegionRepository.cs
@@ -1,6 +1,20 @@
-﻿namespace NZWalks.API.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
 {
     public class SQLRegionRepository : IRegionRepository
     {
+        //inject db context class
+        private readonly NZWalksDbContext dbContext;
+        public SQLRegionRepository(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<List<Region>> GetAllAsync()
+        {
+            return await dbContext.Regions.ToListAsync();
+        }
     }
 }
