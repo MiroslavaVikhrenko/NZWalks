75a896e9efbe04811ac162147ab8f9a8d1af4652
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-12 17:03:20 -0700

    Create an interface IRegionRepository to follow repository pattern

---
+++ NZWalks.API/Repositories/IRegionRepository.cs
@@ -0,0 +1,10 @@
+﻿using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public interface IRegionRepository
+    {
+        //definitions of the 5 methods that we want to expose and later implement in a concrete class
+        Task<List<Region>> GetAllAsync(); //returns a list of region domain models
+    }
+}
