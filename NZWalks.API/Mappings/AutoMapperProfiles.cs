ee404b1ea224592a7b024c28c1b8d2dd7b499294
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-12 18:59:21 -0700

    Add mapping between Region Domain Model and Region DTO (and vice versa) in AutoMapperProfiles

--- NZWalks.API/Mappings/AutoMapperProfiles.cs
+++ NZWalks.API/Mappings/AutoMapperProfiles.cs
@@ -1,8 +1,20 @@
 ﻿using AutoMapper;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
 
 namespace NZWalks.API.Mappings
 {
     public class AutoMapperProfiles : Profile //comes from  AutoMapper
     {
+        public AutoMapperProfiles()
+        {
+            //CreateMap() method needs 2 types - type of source and type of destination
+            //if matches - it will do it for us
+            //if doesn't match - we need to explicitly define the mapping between those properties in these two models
+            //ReverseMap() automatically define reverse map between source and destination
+
+            //Mapping between Region Domain Model and Region DTO (properties' names are the same)
+            CreateMap<Region, RegionDto>().ReverseMap();
+        }
     }
 }
