eb2053bd6038a24c1442a4961b6ff0fed55f3c3c
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-14 15:22:36 -0700

    Create a new Walks endpoint (WalksController)

---
+++ NZWalks.API/Controllers/WalksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    //https://localhost:portnumber/api/walks
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalksController : ControllerBase
+    {
+    }
+}
