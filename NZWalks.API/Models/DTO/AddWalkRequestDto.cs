847e059315e7898945e736b65470d3f61b20271d
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-14 15:29:02 -0700

    Create AddWalkRequestDto for creating walks

---
+++ NZWalks.API/Models/DTO/AddWalkRequestDto.cs
@@ -0,0 +1,16 @@
+﻿namespace NZWalks.API.Models.DTO
+{
+    public class AddWalkRequestDto
+    {
+        //we do not accept Id from the client, the app creates Id by itself
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; } //nullable property
+
+        //properties that set relations between this model and the others:
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+    }
+}
