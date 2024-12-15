9c21867732694be580f0fa3577ef35605c40ea9f
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-15 14:11:17 -0700

    Add data annotations to AddWalkRequestDto for model validations

--- NZWalks.API/Models/DTO/AddWalkRequestDto.cs
+++ NZWalks.API/Models/DTO/AddWalkRequestDto.cs
@@ -1,16 +1,29 @@
-﻿namespace NZWalks.API.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.API.Models.DTO
 {
     public class AddWalkRequestDto
     {
         //we do not accept Id from the client, the app creates Id by itself
-
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0, 50)]
         public double LengthInKm { get; set; }
+
         public string? WalkImageUrl { get; set; } //nullable property
 
         //properties that set relations between this model and the others:
+        [Required]
         public Guid DifficultyId { get; set; }
+
+        [Required]
         public Guid RegionId { get; set; }
     }
 }
