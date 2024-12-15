68c59f129008b086859878a781c145fd78dab878
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-14 17:22:58 -0700

    Create UpdateWalkRequestDto

---
+++ NZWalks.API/Models/DTO/UpdateWalkRequestDto.cs
@@ -0,0 +1,15 @@
+﻿namespace NZWalks.API.Models.DTO
+{
+    public class UpdateWalkRequestDto
+    {
+        //Id is not included as we do not want to allow the client to change it
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; } //nullable property
+
+        //properties that set relations between this model and the others:
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+    }
+}
