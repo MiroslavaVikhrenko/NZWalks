8c961c70c8ff2cdeb949ad8b446ef800fa1d95d0
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-14 18:01:53 -0700

    Complete Delete() action method in WalksController using AutoMapper and repository pattern

--- NZWalks.API/Controllers/WalksController.cs
+++ NZWalks.API/Controllers/WalksController.cs
@@ -89,7 +89,15 @@ namespace NZWalks.API.Controllers
         [Route("{id:Guid}")]
         public async Task<IActionResult> Delete([FromRoute] Guid id)
         {
+            var deletedWalkDomainModel = await walkRepository.DeleteAsync(id);
 
+            if (deletedWalkDomainModel == null)
+            {
+                return NotFound(); //404
+            }
+
+            //Map Domain Model to DTO
+            return Ok(mapper.Map<WalkDto>(deletedWalkDomainModel));
         }
     }
 }
