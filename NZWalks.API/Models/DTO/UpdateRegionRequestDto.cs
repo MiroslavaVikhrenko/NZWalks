024affe88a71a8ddc8234b7d61ed79809f472294
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-15 14:05:34 -0700

    Add data annotations to UpdateRegionRequestDto for model validations

--- NZWalks.API/Models/DTO/UpdateRegionRequestDto.cs
+++ NZWalks.API/Models/DTO/UpdateRegionRequestDto.cs
@@ -1,11 +1,19 @@
-﻿namespace NZWalks.API.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.API.Models.DTO
 {
     public class UpdateRegionRequestDto
     {
         //this class will have only those properties that we want the client to update
         //we do not want to allow the client to update an Id
+
+        [Required] //coming from System.ComponentModel.DataAnnotations
+        [MinLength(3, ErrorMessage = "'Code' has to be minimum of 3 characters")]
+        [MaxLength(3, ErrorMessage = "'Code' has to be maximum of 3 characters")]
         public string Code { get; set; }
+        [Required]
+        [MaxLength(100, ErrorMessage = "'Name' has to be maximum of 100 characters")]
         public string Name { get; set; }
-        public string? RegionImageUrl { get; set; }
+        public string? RegionImageUrl { get; set; } //nullable property
     }
 }
