1a73b79b26d051466242befd24e0c42ff9a35a7c
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-14 17:31:02 -0700

    Add definition of UpdateAsync() method to IWalkRepository interface

--- NZWalks.API/Repositories/IWalkRepository.cs
+++ NZWalks.API/Repositories/IWalkRepository.cs
@@ -8,5 +8,6 @@ namespace NZWalks.API.Repositories
         Task<Walk> CreateAsync(Walk walk); //repositories use domain models
         Task<List<Walk>> GetAllAsync();
         Task<Walk?> GetByIdAsync(Guid id); //Walk could be null (whether this walk is present in the db or not)
+        Task<Walk?> UpdateAsync(Guid id, Walk walk); //Walk is nullable
     }
 }
