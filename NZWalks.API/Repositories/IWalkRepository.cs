3ae1c7040045111239669a1f299bd132059e8507
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-15 17:00:57 -0700

    Change definition for GetAllAsync() method in IWalkRepository interface so that we can implement pagination on results

--- NZWalks.API/Repositories/IWalkRepository.cs
+++ NZWalks.API/Repositories/IWalkRepository.cs
@@ -7,7 +7,8 @@ namespace NZWalks.API.Repositories
         //definitions of the methods
         Task<Walk> CreateAsync(Walk walk); //repositories use domain models
         Task<List<Walk>> GetAllAsync(string? filterOn = null, string? filterQuery = null,
-            string? sortBy = null, bool isAscending = true); //nullabale parameters, by default null
+            string? sortBy = null, bool isAscending = true,
+            int pageNumber = 1, int pageSize = 1000); //nullabale parameters, by default null
         Task<Walk?> GetByIdAsync(Guid id); //Walk could be null (whether this walk is present in the db or not)
         Task<Walk?> UpdateAsync(Guid id, Walk walk); //Walk is nullable
         Task<Walk?> DeleteAsync(Guid id); //Walk is nullable
