299592d616290cee4d8376a1f6494d0f9935523c
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-16 15:00:51 -0700

    Inject UserManager class to AuthController  and create an identity user in Register() method

--- NZWalks.API/Controllers/AuthController.cs
+++ NZWalks.API/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
 
 namespace NZWalks.API.Controllers
 {
@@ -7,12 +9,28 @@ namespace NZWalks.API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        //we need to ingect UserManager class (from Microsoft.AspNetCore.Identity)
+        private readonly UserManager<IdentityUser> userManager;       
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
         //POST: /api/Auth/Register
         [HttpPost]
         [Route("Register")]
-        public async Task<IActionResult> Register()
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
         {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDto.Username,
+                Email = registerRequestDto.Username
+            };
+            var identityResult = await userManager.CreateAsync(identityUser, registerRequestDto.Password);
 
+            if (identityResult.Succeeded)
+            {
+                //Add roles to this User
+            }
         }
     }
 }
