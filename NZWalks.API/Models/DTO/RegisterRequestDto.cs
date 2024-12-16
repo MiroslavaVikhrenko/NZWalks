656d3c1a4f846bb7f385e117f4f93ff6ee451227
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-16 14:52:04 -0700

    Add properties to RegisterRequestDto

--- NZWalks.API/Models/DTO/RegisterRequestDto.cs
+++ NZWalks.API/Models/DTO/RegisterRequestDto.cs
@@ -1,6 +1,14 @@
-﻿namespace NZWalks.API.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks.API.Models.DTO
 {
     public class RegisterRequestDto
     {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Username { get; set; } //username should be an email
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
