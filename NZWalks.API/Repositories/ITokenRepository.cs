67228caf430e1ec11b78b9be6de2e851a32379cc
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-16 15:35:07 -0700

    Create an ITokenRepository interface and add definition for CreateJWTToken() method

---
+++ NZWalks.API/Repositories/ITokenRepository.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NZWalks.API.Repositories
+{
+    public interface ITokenRepository
+    {
+        //IdentityUser comes from Microsoft.AspNetCore.Identity
+        string CreateJWTToken(IdentityUser user, List<string> roles);
+    }
+}
