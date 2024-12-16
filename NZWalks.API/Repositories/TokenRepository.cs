d07078747d2242c0c8c25549b6911bbe789c94a4
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-16 16:03:59 -0700

    Add concrete implementation for CreateJWTToken() method in TokenRepository class

--- NZWalks.API/Repositories/TokenRepository.cs
+++ NZWalks.API/Repositories/TokenRepository.cs
@@ -1,6 +1,45 @@
-﻿namespace NZWalks.API.Repositories
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace NZWalks.API.Repositories
 {
     public class TokenRepository : ITokenRepository
     {
+        //inject IConfiguration to access appsettings.json
+        private readonly IConfiguration configuration;
+
+        public TokenRepository(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public string CreateJWTToken(IdentityUser user, List<string> roles)
+        {
+            //Create claims (Claim comes from System.Security.Claims)
+            var claims = new List<Claim>();
+
+            claims.Add(new Claim(ClaimTypes.Email, user.Email));
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            //Encoding comes from System.Text
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            //create token
+            var token = new JwtSecurityToken(
+                configuration["Jwt:Issuer"],
+                configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token); //returns string
+        }
     }
 }
