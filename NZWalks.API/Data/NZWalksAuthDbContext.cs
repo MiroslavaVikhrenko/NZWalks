e3248b3a2930b66e6124893f0db07d6fd471146c
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-16 13:47:05 -0700

    Create a new db context class for Auth db

---
+++ NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksAuthDbContext : IdentityDbContext //Microsoft.AspNetCore.Identity.EntityFrameworkCore
+    {
+        public NZWalksAuthDbContext(DbContextOptions options) : base(options)
+        {
+            //
+        }
+    }
+}
