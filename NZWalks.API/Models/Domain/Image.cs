f0cc2d933fa53fd231818adc2370410aa4d630bf
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-16 17:36:37 -0700

    Create an Image domain model

---
+++ NZWalks.API/Models/Domain/Image.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NZWalks.API.Models.Domain
+{
+    public class Image
+    {
+        public Guid Id { get; set; }
+        [NotMapped] //excluded from db mapping because we do not want to store this file inside the db
+                    //comes from System.ComponentModel.DataAnnotations.Schema
+        public IFormFile File { get; set; }
+        public string FileName { get; set; }
+        public string? FileDescription { get; set; } //optional property
+        public string FileExtension { get; set; } //.jpg, .png etc
+        public long FileSizeInBytes { get; set; }
+        public string FilePath { get; set; }
+    }
+}
