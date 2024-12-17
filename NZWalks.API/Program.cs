6ee0d120ddd0e376ae5e6124d8cf35bb4741a8a5
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-17 16:51:44 -0700

    Inject Serilog for logging in Program.cs file

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -8,10 +8,20 @@ using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.OpenApi.Models;
 using Microsoft.Extensions.FileProviders;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container. (Inject services)
+
+//Inject Serilog for logging - LoggerConfiguration comes from Serilog
+var logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .MinimumLevel.Information()
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
 builder.Services.AddControllers();
 
