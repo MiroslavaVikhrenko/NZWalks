953f5a2af05adab9b4159cd80b289ec10543dd43
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-17 14:39:39 -0700

    Create a class LocalImageRepository which implements interface IImageRepository

---
+++ NZWalks.API/Repositories/LocalImageRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace NZWalks.API.Repositories
+{
+    //we are uploading images to a local solution or a local path and not to any third party solutions for example Cloudinary
+    public class LocalImageRepository : IImageRepository
+    {
+    }
+}
