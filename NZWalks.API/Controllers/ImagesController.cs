4bd99b30a3eb65a8f6e7d7dc9dfcad8aad7cfbf4
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-16 18:12:50 -0700

    Add a private ValidateFileUpload() method to check the extension and size of uploded file in ImagesController

--- NZWalks.API/Controllers/ImagesController.cs
+++ NZWalks.API/Controllers/ImagesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
 
 namespace NZWalks.API.Controllers
 {
@@ -10,10 +11,26 @@ namespace NZWalks.API.Controllers
         //POST: /api/Images/Upload
         [HttpPost]
         [Route("Upload")]
-        public async Task<IActionResult> Upload([FromForm])
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto request)
         {
-
+            //Validate if this request is correct or not
         }
 
+        private void ValidateFileUpload(ImageUploadRequestDto request)
+        {
+            //Validate extension and size
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+
+            if (!allowedExtensions.Contains(Path.GetExtension(request.File.FileName)))
+            {
+                //pass the key as 'file' and error message
+                ModelState.AddModelError("file", "Unsupported file extension");
+            }
+            //10 MB = 10485760 Bytes
+            if (request.File.Length > 10485760)
+            {
+                ModelState.AddModelError("file", "File size more than 10 MB, please upload a smaller size file.");
+            }
+        }
     }
 }
