4e31566495fbb61f1ebe5c3767355111b9062c79
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-17 17:53:55 -0700

    Inject RequestDelegate in ExceptionHandlerMiddleware class

--- NZWalks.API/Middlewares/ExceptionHandlerMiddleware.cs
+++ NZWalks.API/Middlewares/ExceptionHandlerMiddleware.cs
@@ -2,11 +2,16 @@
 {
     public class ExceptionHandlerMiddleware
     {
-        //Inject ILogger for logging
-        private readonly ILogger<ExceptionHandlerMiddleware> logger;   
-        public ExceptionHandlerMiddleware(ILogger<ExceptionHandlerMiddleware> logger)
+        //Inject ILogger for logging and RequestDelegate
+        //RequestDelegate returns a task that represents the completion of a request processing and it's a function that can process an HTTP request
+        private readonly ILogger<ExceptionHandlerMiddleware> logger;
+        private readonly RequestDelegate next;
+
+        public ExceptionHandlerMiddleware(ILogger<ExceptionHandlerMiddleware> logger,
+            RequestDelegate next)
         {
             this.logger = logger;
+            this.next = next;
         }
     }
 }
