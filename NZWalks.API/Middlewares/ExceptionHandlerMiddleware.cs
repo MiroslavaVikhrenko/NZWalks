e22f519673d33864c1aaf8e813f6a80f25959718
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-17 18:10:48 -0700

    Complete InvokeAsync() method in ExceptionHandlerMiddleware class for Global Exception Handling

--- NZWalks.API/Middlewares/ExceptionHandlerMiddleware.cs
+++ NZWalks.API/Middlewares/ExceptionHandlerMiddleware.cs
@@ -1,4 +1,6 @@
-﻿namespace NZWalks.API.Middlewares
+﻿using System.Net;
+
+namespace NZWalks.API.Middlewares
 {
     public class ExceptionHandlerMiddleware
     {
@@ -12,6 +14,37 @@
         {
             this.logger = logger;
             this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            //mimic try-catch block that we wish to add to every API
+            try
+            {
+                await next(httpContext); //if anything happens during the calls,
+                                         //we want to log the exceptions or
+                                         //handle the exceptions at a single location (= in the catch block below)
+            }
+            catch (Exception ex)
+            {
+                //we want to send a unique identifier to the log file and send as part of the response
+                var errorId = Guid.NewGuid();
+
+                //Log this exception
+                logger.LogError(ex,$"{errorId} : {ex.Message}");
+                
+                //Return a custom error response for consistency
+                httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError; //500
+                httpContext.Response.ContentType = "application/json";
+
+                var error = new
+                {
+                    Id = errorId,
+                    ErrorMessage = "Something went wrong. We are looking into resolving this."
+                };
+
+                await httpContext.Response.WriteAsJsonAsync(error);
+            }
         }
     }
 }
