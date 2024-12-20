b5196ceaf78b9ec90d4379e59a04bd6af3403e41
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-20 14:33:32 -0700

    Create a WalksController

---
+++ NZWalks.UI/Controllers/WalksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.UI.Controllers
+{
+    public class WalksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
