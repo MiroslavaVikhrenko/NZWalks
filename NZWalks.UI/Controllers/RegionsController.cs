723e850d057c5f89ade8a39f90b2bb7fdf42ed0c
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-20 13:29:58 -0700

    Add Delete() action method to RegionsController

--- NZWalks.UI/Controllers/RegionsController.cs
+++ NZWalks.UI/Controllers/RegionsController.cs
@@ -114,5 +114,25 @@ namespace NZWalks.UI.Controllers
             }
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(RegionDto request)
+        {
+            try
+            {
+                var client = httpClientFactory.CreateClient();
+
+                var httpResponseMessage = await client.DeleteAsync($"https://localhost:7008/api/regions/{request.Id}");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                return RedirectToAction("Index", "Regions");
+            }
+            catch (Exception ex)
+            {
+                //Log the exception               
+            }
+            return View("Edit");
+        }
     }
 }
