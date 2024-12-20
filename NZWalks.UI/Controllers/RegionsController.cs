bd396a36bc19885092d50e2485edfaa4e432010c
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-19 17:32:39 -0700

    Inject HttpClientFactory in RegionsController for NZWalks.UI project

--- NZWalks.UI/Controllers/RegionsController.cs
+++ NZWalks.UI/Controllers/RegionsController.cs
@@ -4,6 +4,13 @@ namespace NZWalks.UI.Controllers
 {
     public class RegionsController : Controller
     {
+        // Inject HttpClientFactory
+        private readonly IHttpClientFactory httpClientFactory;      
+
+        public RegionsController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index()
         {
             //Get all regions from Web API (and then pass this information to the View to display it)
