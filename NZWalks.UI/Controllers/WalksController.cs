c01a14fdfd8f417aea17f728612b94d922ecaa76
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-20 15:33:20 -0700

    Implement the same functionality as in RegionsController in WalksController

--- NZWalks.UI/Controllers/WalksController.cs
+++ NZWalks.UI/Controllers/WalksController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.UI.Models;
 using NZWalks.UI.Models.DTO;
+using System.Text.Json;
+using System.Text;
 
 namespace NZWalks.UI.Controllers
 {
@@ -34,6 +37,96 @@ namespace NZWalks.UI.Controllers
                 throw;
             }
             return View(response);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddWalkViewModel model)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://localhost:7008/api/walks"),
+                Content = new StringContent(JsonSerializer.Serialize(model), Encoding.UTF8, "application/json")
+            };
+
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var response = await httpResponseMessage.Content.ReadFromJsonAsync<WalkDto>();
+
+            if (response is not null)
+            {
+                return RedirectToAction("Index", "Walks");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var response = await client.GetFromJsonAsync<UpdateWalkRequestDto>($"https://localhost:7008/api/walks/{id.ToString()}");
+
+            if (response is not null)
+            {
+                return View(response);
+            }
+
+            return View(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(UpdateWalkRequestDto request)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var httpRequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Put,
+                RequestUri = new Uri($"https://localhost:7008/api/walks/{request.Id}"),
+                Content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json")
+            };
+
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var response = await httpResponseMessage.Content.ReadFromJsonAsync<UpdateWalkRequestDto>();
+
+            if (response is not null)
+            {
+                return RedirectToAction("Edit", "Walks");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(WalkDto request)
+        {
+            try
+            {
+                var client = httpClientFactory.CreateClient();
+
+                var httpResponseMessage = await client.DeleteAsync($"https://localhost:7008/api/walks/{request.Id}");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                return RedirectToAction("Index", "Walks");
+            }
+            catch (Exception ex)
+            {
+                //Log the exception               
+            }
+            return View("Edit");
         }
     }
 }
